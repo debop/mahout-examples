b39f7796099f5723ed670823548b9684bab2077e
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2014-12-17 18:36:12 +0900

    -. 추천 예제 추가 중.

--- src/test/scala/kr/debop/mahout/examples/recommender/ch02/EvaluatorIntroFunSuite.scala
+++ src/test/scala/kr/debop/mahout/examples/recommender/ch02/EvaluatorIntroFunSuite.scala
@@ -14,6 +14,8 @@ import org.apache.mahout.cf.taste.recommender.Recommender
 import org.apache.mahout.common.RandomUtils
 import org.slf4j.LoggerFactory
 
+import scala.collection.JavaConverters._
+
 /**
  * EvaluatorIntroFunSuite
  * @author sunghyouk.bae@gmail.com
@@ -22,7 +24,23 @@ class EvaluatorIntroFunSuite extends AbstractMahoutFunSuite {
 
   private val log = LoggerFactory.getLogger(getClass)
 
-  test("Evaluator 소개") {
+  test("RecommendIntro") {
+    val dataFile = new File("src/data/intro.csv")
+    val model = new FileDataModel(dataFile)
+
+    val similarity = new PearsonCorrelationSimilarity(model)
+    val neighborhood = new NearestNUserNeighborhood(2, similarity, model)
+
+    val recommender = new GenericUserBasedRecommender(model, neighborhood, similarity)
+
+    val recommendations = recommender.recommend(1, 2)
+
+    recommendations.asScala.foreach { recommendation =>
+      println(recommendation)
+    }
+  }
+
+  test("추천기 평가하기. 실제와 예측값의 차이로 평가") {
     RandomUtils.useTestSeed()
 
     val dataFile = new File("src/data/intro.csv")
@@ -38,8 +56,12 @@ class EvaluatorIntroFunSuite extends AbstractMahoutFunSuite {
       }
     }
     // Use 70% of the data to train; test using the other 30%.
-    val score = evaluator.evaluate(recommenderBuilder, null, model, 0.9, 1.0)
-    log.debug(s"Score=$score")
+    val score = evaluator.evaluate(recommenderBuilder, null, model, 0.7, 3.0)
+    println(s"Score=$score")
+  }
+
+  test("") {
+
   }
 
 }
