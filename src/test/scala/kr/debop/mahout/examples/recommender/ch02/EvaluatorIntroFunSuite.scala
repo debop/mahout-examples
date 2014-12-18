2492a33aff8b5559067253b02667fbf938871faf
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2014-12-18 10:08:38 +0900

    -. 추천 정확율, 재현율 공부...

--- src/test/scala/kr/debop/mahout/examples/recommender/ch02/EvaluatorIntroFunSuite.scala
+++ src/test/scala/kr/debop/mahout/examples/recommender/ch02/EvaluatorIntroFunSuite.scala
@@ -4,7 +4,7 @@ import java.io.File
 
 import kr.debop.mahout.examples.AbstractMahoutFunSuite
 import org.apache.mahout.cf.taste.eval.RecommenderBuilder
-import org.apache.mahout.cf.taste.impl.eval.AverageAbsoluteDifferenceRecommenderEvaluator
+import org.apache.mahout.cf.taste.impl.eval.{AverageAbsoluteDifferenceRecommenderEvaluator, GenericRecommenderIRStatsEvaluator}
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel
 import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood
 import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender
@@ -49,19 +49,47 @@ class EvaluatorIntroFunSuite extends AbstractMahoutFunSuite {
     val evaluator = new AverageAbsoluteDifferenceRecommenderEvaluator()
     val recommenderBuilder = new RecommenderBuilder {
       override def buildRecommender(dataModel: DataModel): Recommender = {
-        val similarity = new PearsonCorrelationSimilarity(model)
-        val neighborhood = new NearestNUserNeighborhood(2, similarity, model)
+        val similarity = new PearsonCorrelationSimilarity(dataModel)
+        val neighborhood = new NearestNUserNeighborhood(2, similarity, dataModel)
 
-        new GenericUserBasedRecommender(model, neighborhood, similarity)
+        new GenericUserBasedRecommender(dataModel, neighborhood, similarity)
       }
     }
     // Use 70% of the data to train; test using the other 30%.
-    val score = evaluator.evaluate(recommenderBuilder, null, model, 0.7, 3.0)
+    val score = evaluator.evaluate(recommenderBuilder, null, model, 0.7, 1.0)
     println(s"Score=$score")
   }
 
-  test("") {
+  test("추천기 정확율/재현율") {
+    RandomUtils.useTestSeed()
 
+    val dataFile = new File("src/data/intro.csv")
+    val model = new FileDataModel(dataFile)
+
+    val evaluator = new GenericRecommenderIRStatsEvaluator()
+
+    // Build the same recommender for testing that we did last time:
+    val recommenderBuilder = new RecommenderBuilder {
+      override def buildRecommender(dataModel: DataModel): Recommender = {
+        val similarity = new PearsonCorrelationSimilarity(dataModel)
+        val neighborhood = new NearestNUserNeighborhood(2, similarity, dataModel)
+
+        new GenericUserBasedRecommender(dataModel, neighborhood, similarity)
+      }
+    }
+
+    // Evaluate precision and recall "at 2":
+    val stats = evaluator.evaluate(recommenderBuilder,
+                                    null,
+                                    model,
+                                    null,
+                                    2,
+                                    GenericRecommenderIRStatsEvaluator.CHOOSE_THRESHOLD,
+                                    1.0)
+
+    println(s"정확율(precision)=${stats.getPrecision}")
+    println(s"재현율(recall)=${stats.getRecall}")
+    println(s"stats=$stats")
   }
 
 }
